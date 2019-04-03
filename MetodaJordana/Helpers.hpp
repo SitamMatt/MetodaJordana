25ce44b66c2fca64069525b46adfdee6be7ec7a1
SitamMatt <mateuszszewc@op.pl>
2019-04-03 11:59:27 +0200

    #

--- MetodaJordana/Helpers.hpp
+++ MetodaJordana/Helpers.hpp
@@ -1,11 +1,55 @@
 #include <iostream>
 #include <iomanip>
 
+
+
 void PrintMatrix(double matrix[][4], int M, int N) {
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < N; j++) {
-			std::cout << std::setw(10) << matrix[i][j] << "||||";
+			std::cout << std::setw(10) << matrix[i][j] << "|";
 		}
 		std::cout << std::endl;
+	}
+}
+
+void reduceToDiagonal(double matrix[3][4], int m, int n) {
+	int br = 0;
+	for (int i = 0; i < m; i++)
+	{
+		if (matrix[i][i] == 0)
+		{
+			br++;
+		}
+		else
+		{
+			for (int k = 0; k < m; k++)
+			{
+				if (k != i) {
+
+					double multiplier = matrix[k][i] / matrix[i][i];
+
+					for (int j = 0; j < n; j++)
+					{
+						matrix[k][j] -= matrix[i][j] * multiplier;
+					}
+				}
+			}
+		}
+	}
+	std::cout << br << std::endl;
+	PrintMatrix(matrix, m, n);
+}
+
+void reduceToUnit(double matrix[3][4], int m, int n) {
+	for (int i = 0; i < m; i++)
+	{
+		double divider = matrix[i][i];
+		if (divider != 0) 
+		{
+			for (int j = 0; j < n; j++)
+			{
+				matrix[i][j] /= divider;
+			}
+		}
 	}
 }
--- MetodaJordana/main.cpp
+++ MetodaJordana/main.cpp
@@ -10,49 +10,52 @@ using namespace std;
 
 int main() {
 	double mat[3][4] = {
-		{1,-1,1, -4},
-		{8,2.21,-5, -10},
-		{4,1,1, 2}
+		{3,3,1, 1},
+		{2,5, 7,20},
+		{-4,-10,-14, -20},
 	};
 	int n = 3;
 	PrintMatrix(mat, 3, 4);
 	cout << endl << endl;
 
+	reduceToDiagonal(mat, 3, 4);
+	//reduceToUnit(mat, 3, 4);
+	//for (int i = 0; i < n; i++) {
 
-	for (int i = 0; i < n; i++) {
+	//	// pierwszy niezerowy wspó³czynnik i-tego wiersza
+	//	double divider = mat[i][i];
 
-		// i,i element rownania
-		double a = mat[i][i];
-		// dzielenie wiersza przez jego i,i element
-		for (int j = 0; j < 4; j++) {
-			mat[i][j] /= a;
-			/*system("cls");
-			PrintMatrix(mat, 3, 4);
-			_getch();*/
-		}
-		// odejmowanie wierszy i mnozenie
-		for (int k = 0; k < n; k++) {
-			
+	//	// dzielenie i-tego wiersza przez dzielnik 
+	//	for (int j = 0; j < 4; j++) {
+	//		mat[i][j] /= divider;
+	//		system("cls");
+	//		PrintMatrix(mat, 3, 4);
+	//		_getch();
+	//	}
 
-			// pomijamy wiersz obecny
-			if (k != i) {
-				//int wiers_razy_a[4];
-				// mnozenie aktualnego wiersza przez k,i
-				/*for (int l = 0; l < 4; l++) {
-					wiers_razy_a[l] = mat[i][l] * mat[k][i];
-				}*/
-				double b = mat[k][i] / mat[i][i];
-				// odejmowanie wiersza
-				for (int m = 0; m < 4; m++) {
-					mat[k][m] -= mat[i][m] * b;
-					/*system("cls");
-					PrintMatrix(mat, 3, 4);
-					_getch();*/
-				}
-			}
-		}
-	}
-	PrintMatrix(mat, 3, 4);
+	//	// redukcja 
+	//	for (int k = 0; k < n; k++) {
+	//		
+
+	//		// pomijamy wiersz obecny
+	//		if (k != i) {
+	//			//int wiers_razy_a[4];
+	//			// mnozenie aktualnego wiersza przez k,i
+	//			/*for (int l = 0; l < 4; l++) {
+	//				wiers_razy_a[l] = mat[i][l] * mat[k][i];
+	//			}*/
+	//			double b = mat[k][i] / mat[i][i];
+	//			// odejmowanie wiersza
+	//			for (int m = 0; m < 4; m++) {
+	//				mat[k][m] -= mat[i][m] * b;
+	//				system("cls");
+	//				PrintMatrix(mat, 3, 4);
+	//				_getch();
+	//			}
+	//		}
+	//	}
+	//}
+	//PrintMatrix(mat, 3, 4);
 
 	system("pause");
 	return 0;
